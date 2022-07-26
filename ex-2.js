51b15c220a97c7d241162e78136ec660774ed58b
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-26 12:59:20 +0700

    Update ex-2.js

--- ex-2.js
+++ ex-2.js
@@ -1,8 +1,10 @@
+// Exercise #2 Product
+
 let product = {
   name: "Xiaomi Air Purifier",
   price: 4000,
   ratings: 2.5,
   photo: null,
 };
-
 let socialMediaShare = "facebookShare";
+// Start code here
