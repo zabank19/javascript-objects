a5ee093c2fff1cb899c2f4177c6da9ffdeab8a14
Mu <muvonglao@gmail.com>
2023-06-30 14:25:37 +0700

    feat: added debugging ex

---
+++ ex-3.js
@@ -0,0 +1,29 @@
+// Exercise #3 Debugging
+
+// Start coding here
+const employee = {
+  name: "John Doe",
+  age: 18,
+  address: {
+    street: "123 Main Street",
+    city: "Cityville",
+    states: "Stateville",
+    country: "Countryland",
+    postalCode: "12345",
+  },
+};
+
+console.log("Employee Name: " + employees.name);
+console.log("Employee Age: " + employee.age);
+console.log(
+  "Employee Address: " +
+    employee.address.street +
+    ", " +
+    employee.address.city +
+    ", " +
+    employee.address.state +
+    ", " +
+    employee.address.country +
+    " " +
+    employee.adress.postalCode
+);
